cd2ace0975069639834e656125bd7986b73b19e9
fiapfelipe <rm559175@fiap.com.br>
2025-06-08 19:47:50 -0300

    componente rodapé

---
+++ app/Components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t-2 border-t-blue-300 bg-white">
+      <div className="container mx-auto py-3 px-6">
+        <p className="font-semibold text-blue-500 text-sm text-center">Copyright © <span className="font-bold">SOS, 2025</span></p>
+      </div>
+    </footer>
+  )
+}
